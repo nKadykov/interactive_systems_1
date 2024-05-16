553f744c16f729c5e56664bba77be97deb1a037f
nKadykov <nkadykov@gmail.com>
2024-05-16 14:11:40 +0300

    init

--- keyboard.cpp
+++ keyboard.cpp
@@ -4,16 +4,25 @@
 #include <QGridLayout>
 #include <QLineEdit>
 
-Keyboard::Keyboard(QWidget* parent) : QWidget(parent) {
+Keyboard::Keyboard(QWidget* parent) : QWidget(parent), m_timer(new QTimer(this)) {
+    m_timer->setInterval(1000);
+    m_timer->start(1000);
+    int time = rand() % 10;
     m_display = new QLineEdit("0");
     m_display->setReadOnly(true);
     m_display->setAlignment(Qt::AlignRight);
+    m_color.setBlue(1);
+    m_palette.setColor(QPalette::Button, QColor(Qt::blue));
 
     for(int i = 0; i < NUMBER; ++i) {
         m_buttons[i] = createButton(QString::number(i), &Keyboard::clickButton);
+        m_buttons[i]->setPalette(m_palette);
+        m_buttons[i]->setStyleSheet("background-color: white");
+        m_color = QColor(Qt::white);
     }
 
     QGridLayout *mainLayout = new QGridLayout;
+    mainLayout->setSizeConstraint(QLayout::SetMaximumSize);
     mainLayout->addWidget(m_display, 0, 0);
     for(int i = 0; i < 10; ++i) {
         mainLayout->addWidget(m_buttons[i], 1, i);
@@ -32,5 +41,14 @@ Button *Keyboard::createButton(const QString &text, const PointerToMemberFunctio
 void Keyboard::clickButton() {
     Button *clicked_button = qobject_cast<Button*>(sender());
     int digitValue = clicked_button->text().toInt();
+    m_color = clicked_button->palette().color(QPalette::Button);
+    if(m_color == QColor(Qt::white)) {
+        clicked_button->setStyleSheet("background-color: red");
+        m_color = QColor(Qt::red);
+    }
+    else if(m_color == QColor(Qt::red)) {
+        clicked_button->setStyleSheet("background-color: white");
+        m_color = QColor(Qt::white);
+    }
     m_display->setText(QString::number(digitValue));
 }
--- keyboard.h
+++ keyboard.h
@@ -1,6 +1,9 @@
 #ifndef KEYBOARD_H
 #define KEYBOARD_H
 #include <QWidget>
+#include <QTimer>
+#include <QPalette>
+#include <QColor>
 
 QT_BEGIN_NAMESPACE
 class QLineEdit;
@@ -20,6 +23,9 @@ private:
     QLineEdit *m_display;
     enum {NUMBER = 10};
     Button *m_buttons[NUMBER];
+    QTimer *m_timer;
+    QColor m_color;
+    QPalette m_palette;
 };
 
 #endif // KEYBOARD_H
